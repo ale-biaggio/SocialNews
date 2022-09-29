fb11c9e0f793d073b0bbfa1bf15178122c1a90b8
ale-biaggio <alessandrobiaggioli@gmail.com>
2022-09-29 11:39:22 +0200

    tag->category

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,6 @@ class Post < ApplicationRecord
 
     validates :title, :presence=> true, length: {in: 2..50 }
     validates :body, :presence=> true, length: {in: 100..1000}
+    validates :category, :presence=> true
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_27_171433) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_29_085923) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -41,7 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_171433) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "tag"
+    t.string "category"
     t.text "body"
     t.integer "rank"
     t.datetime "created_at", null: false
