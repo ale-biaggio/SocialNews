6f49ae1af950c902f0ab4527653f9fb4d649f0a9
ale-biaggio <alessandrobiaggioli@gmail.com>
2022-09-29 12:47:20 +0200

    debugging

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,10 @@ class Post < ApplicationRecord
     #model validation
 
     has_one_attached :image
-
+    
+    def self.all_category ; %w[Affari Intrattenimento Politica Salute Spettacolo Sport Tecnologia] ; end
     validates :title, :presence=> true, length: {in: 2..50 }
     validates :body, :presence=> true, length: {in: 100..1000}
-    validates :category, :presence=> true
+    validates :category, :presence=> true, :inclusion => {:in => Post.all_category}
 
 end
