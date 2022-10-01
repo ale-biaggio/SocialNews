2f129b9c81c9da89f2e8b5945c52d6306cb516a2
ale-biaggio <alessandrobiaggioli@gmail.com>
2022-10-01 20:00:39 +0200

    Revert "Revert "debugging""
    This reverts commit 479ea4c0545eb40aee3f83f3adfec7c691d84432.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,10 @@ class Post < ApplicationRecord
 
 >>>>>>> parent of 8a48ee5 (model di user e commenti)
     has_one_attached :image
-
+    
+    def self.all_category ; %w[Affari Intrattenimento Politica Salute Spettacolo Sport Tecnologia] ; end
     validates :title, :presence=> true, length: {in: 2..50 }
     validates :body, :presence=> true, length: {in: 100..1000}
-    validates :category, :presence=> true
+    validates :category, :presence=> true, :inclusion => {:in => Post.all_category}
 
 end
