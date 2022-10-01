479ea4c0545eb40aee3f83f3adfec7c691d84432
Martinaa9 <103061943+Martinaa9@users.noreply.github.com>
2022-10-01 18:24:27 +0200

    Revert "debugging"
    This reverts commit 6f49ae1af950c902f0ab4527653f9fb4d649f0a9.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,9 @@ class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     
     has_one_attached :image
-    
-    def self.all_category ; %w[Affari Intrattenimento Politica Salute Spettacolo Sport Tecnologia] ; end
+
     validates :title, :presence=> true, length: {in: 2..50 }
     validates :body, :presence=> true, length: {in: 100..1000}
-    validates :category, :presence=> true, :inclusion => {:in => Post.all_category}
+    validates :category, :presence=> true
 
 end
