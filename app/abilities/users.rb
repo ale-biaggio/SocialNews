8da098620a1f594cac2d76ea291e7faa239e33e0
gabrielecireddu <gabrielecireddu@gmail.com>
2022-10-17 17:17:30 +0200

    controlli current_user

--- app/abilities/users.rb
+++ app/abilities/users.rb
@@ -1,5 +1,5 @@
 Canard::Abilities.for(:user) do
   #inserire anche post e user
-  can [:read ,  :update , :create , :new , :edit, :destroy], Comment
-  cannot [ ], Comment
+  can [:read ,  :update , :create , :new ], Comment
+  cannot [ :edit, :destroy ], Comment
 end
