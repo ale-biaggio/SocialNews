1cdf0258ac112a770359da76ac13c7088aa469ea
ale-biaggio <alessandrobiaggioli@gmail.com>
2022-12-10 17:22:53 +0100

    api  repubblica completate

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -3,16 +3,13 @@ class OperationsController < ApplicationController
     def save_posts
         if GoogleNews.save_posts_from_google
             @message = 'Succeed'
-            #repubblica = HTTP.get("https://newsapi.org/v2/everything?sources=la-repubblica&sortBy=popularity&apiKey=4d5c3cbe2fd54b948ccabe755b9d6d38")
-            #ee = repubblica.parse
-            #n=0
-            #while n<1000
-            #    @message=ee["articles"]#[n]["url"].split("/")[3]
-            #    puts @message
-            #    n += 1
+            #repubblica = HTTP.get("https://newsapi.org/v2/everything?sources=la-repubblica&sortBy=publishedAt&apiKey=4d5c3cbe2fd54b948ccabe755b9d6d38")
+            #@message = repubblica.parse
+            ###@message=ee["articles"]#[n]["url"].split("/")[3]
+            #@len = @message["totalResults"]
+            #@message['articles'].each do |article|
+            #    puts article
             #end
-            ##@message=ee
-            ##puts @message
         else
             @message = 'Failed'
         end
--- app/services/google_news.rb
+++ app/services/google_news.rb
@@ -6,7 +6,6 @@ class GoogleNews
         attempts = 0
         begin
             attempts += 1
-
             repubblica = HTTP.get("https://newsapi.org/v2/everything?sources=la-repubblica&sortBy=popularity&apiKey=5e8be9cf55d3442883b6a248fbc8ab4b")
             ansa = HTTP.get("https://newsapi.org/v2/everything?sources=ansa&sortBy=popularity&apiKey=5e8be9cf55d3442883b6a248fbc8ab4b")
             sole24 = HTTP.get("https://newsapi.org/v2/everything?sources=il-sole-24-ore&sortBy=popularity&apiKey=5e8be9cf55d3442883b6a248fbc8ab4b")
@@ -24,68 +23,65 @@ class GoogleNews
             end
         end
         #salvataggio nel db degli articoli di repubblica 
-        rep_json = repubblica.parse
-        tot_res = rep_json["totalResults"]
-        n = 0
-        affari, cultura, politica, spettacolo, sport = 0,0,0,0,0
-        while (n < tot_res - 1)
-            keyword = rep_json["articles"][n]["url"].split("/")[3]
-            if (affari < 3 && keyword == "economia")
-                m = Post.new
-                m['user_id'] = 1
-                m['title'] = rep_json["articles"][n]["title"]
-                m['category'] = 'Affari'
-                m['body'] = rep_json["articles"][n]["content"]
-                m['rank'] = n
-                m.save!
-                affari += 1
-            end
-            if (cultura < 3 && keyword == "cultura")
-                m = Post.new
-                m['user_id'] = 1
-                m['title'] = rep_json["articles"][n]["title"]
-                m['category'] = 'Cultura'
-                m['body'] = rep_json["articles"][n]["content"]
-                m['rank'] = n
-                m.save!
-                cultura += 1
-            end
-            if (politica < 3 && keyword == "politica")
-                m = Post.new
-                m['user_id'] = 1
-                m['title'] = rep_json["articles"][n]["title"]
-                m['category'] = 'Politica'
-                m['body'] = rep_json["articles"][n]["content"]
-                m['rank'] = n
-                m.save!
-                politica += 1
-            end
-            if (spettacolo < 3 && keyword == "spettacoli")
-                m = Post.new
-                m['user_id'] = 1
-                m['title'] = rep_json["articles"][n]["title"]
-                m['category'] = 'Spettacoli'
-                m['body'] = rep_json["articles"][n]["content"]
-                m['rank'] = n
-                m.save!
-                spettacolo += 1
-            end
-            if (sport < 3 && keyword == "sport")
-                m = Post.new
-                m['user_id'] = 1
-                m['title'] = rep_json["articles"][n]["title"]
-                m['category'] = 'Affari'
-                m['body'] = rep_json["articles"][n]["content"]
-                m['rank'] = n
-                m.save!
-                sport += 1
-            end
-            if(affari == 3 && cultura == 3 && politica == 3 && spettacolo == 3 && sport == 3)
-                n = tot_res
-            else
-                n += 1
-            end
-        end
+       rep_json = repubblica.parse
+       tot_res = rep_json["totalResults"]
+       n = 18
+       affari, intrattenimento, politica, spettacolo, sport = 0,0,0,0,0
+
+       rep_json['articles'].each do |article|
+           keyword = article["url"].split("/")[3]
+           if (affari < 3 && keyword == "economia")
+               m = Post.new
+               m['user_id'] = 1
+               m['title'] = article["title"]
+               m['category'] = 'Affari'
+               m['body'] = article["content"]
+               m['rank'] = n
+               m.save!
+               affari += 1
+           end
+           if (intrattenimento < 3 && keyword == "cultura")
+               m = Post.new
+               m['user_id'] = 1
+               m['title'] = article["title"]
+               m['category'] = 'Intrattenimento'
+               m['body'] = article["content"]
+               m['rank'] = n
+               m.save!
+               intrattenimento += 1
+           end
+           if (politica < 3 && keyword == "politica")
+               m = Post.new
+               m['user_id'] = 1
+               m['title'] = article["title"]
+               m['category'] = 'Politica'
+               m['body'] = article["content"]
+               m['rank'] = n
+               m.save!
+               politica += 1
+           end
+           if (spettacolo < 3 && keyword == "spettacoli")
+               m = Post.new
+               m['user_id'] = 1
+               m['title'] = article["title"]
+               m['category'] = 'Spettacolo'
+               m['body'] = article["content"]
+               m['rank'] = n
+               m.save!
+               spettacolo += 1
+           end
+           if (sport < 3 && keyword == "sport")
+               m = Post.new
+               m['user_id'] = 1
+               m['title'] = article["title"]
+               m['category'] = 'Sport'
+               m['body'] = article["content"]
+               m['rank'] = n
+               m.save!
+               sport += 1
+           end
+           n -= 1
+       end
         return true
     end
 
