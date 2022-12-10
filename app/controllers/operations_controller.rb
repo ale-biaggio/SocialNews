7833aaf70b08bd417d47f972e87b0c53d76f7d9b
ale-biaggio <alessandrobiaggioli@gmail.com>
2022-12-10 12:38:14 +0100

    commit provvisorio api

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -3,6 +3,16 @@ class OperationsController < ApplicationController
     def save_posts
         if GoogleNews.save_posts_from_google
             @message = 'Succeed'
+            #repubblica = HTTP.get("https://newsapi.org/v2/everything?sources=la-repubblica&sortBy=popularity&apiKey=4d5c3cbe2fd54b948ccabe755b9d6d38")
+            #ee = repubblica.parse
+            #n=0
+            #while n<1000
+            #    @message=ee["articles"]#[n]["url"].split("/")[3]
+            #    puts @message
+            #    n += 1
+            #end
+            ##@message=ee
+            ##puts @message
         else
             @message = 'Failed'
         end
--- app/services/google_news.rb
+++ app/services/google_news.rb
@@ -7,11 +7,11 @@ class GoogleNews
         begin
             attempts += 1
 
-            repubblica = HTTP.get("https://newsapi.org/v2/everything?sources=la-repubblica&sortBy=popularity&apiKey=4d5c3cbe2fd54b948ccabe755b9d6d38")
-            ansa = HTTP.get("https://newsapi.org/v2/everything?sources=ansa&sortBy=popularity&apiKey=4d5c3cbe2fd54b948ccabe755b9d6d38")
-            sole24 = HTTP.get("https://newsapi.org/v2/everything?sources=il-sole-24-ore&sortBy=popularity&apiKey=4d5c3cbe2fd54b948ccabe755b9d6d38")
-            football = HTTP.get("https://newsapi.org/v2/everything?sources=football-italia&sortBy=popularity&apiKey=4d5c3cbe2fd54b948ccabe755b9d6d38")
-            bbc = HTTP.get("https://newsapi.org/v2/everything?q=italy&source=bbc-news&sortBy=popularity&apiKey=4d5c3cbe2fd54b948ccabe755b9d6d38")
+            repubblica = HTTP.get("https://newsapi.org/v2/everything?sources=la-repubblica&sortBy=popularity&apiKey=5e8be9cf55d3442883b6a248fbc8ab4b")
+            ansa = HTTP.get("https://newsapi.org/v2/everything?sources=ansa&sortBy=popularity&apiKey=5e8be9cf55d3442883b6a248fbc8ab4b")
+            sole24 = HTTP.get("https://newsapi.org/v2/everything?sources=il-sole-24-ore&sortBy=popularity&apiKey=5e8be9cf55d3442883b6a248fbc8ab4b")
+            football = HTTP.get("https://newsapi.org/v2/everything?sources=football-italia&sortBy=popularity&apiKey=5e8be9cf55d3442883b6a248fbc8ab4b")
+            bbc = HTTP.get("https://newsapi.org/v2/everything?q=italy&source=bbc-news&sortBy=popularity&apiKey=5e8be9cf55d3442883b6a248fbc8ab4b")
         
         rescue => e
             if attempts < 10
@@ -23,15 +23,69 @@ class GoogleNews
                 return false
             end
         end
-
-
-        p=response.parse
-        m = Post.new
-        m['user_id']=1
-        m['title']=p['articles'][1]['title']
-        m['category']='Salute'
-        m['body']=p['articles'][1]['content']
-        m.save!
+        #salvataggio nel db degli articoli di repubblica 
+        rep_json = repubblica.parse
+        tot_res = rep_json["totalResults"]
+        n = 0
+        affari, cultura, politica, spettacolo, sport = 0,0,0,0,0
+        while (n < tot_res - 1)
+            keyword = rep_json["articles"][n]["url"].split("/")[3]
+            if (affari < 3 && keyword == "economia")
+                m = Post.new
+                m['user_id'] = 1
+                m['title'] = rep_json["articles"][n]["title"]
+                m['category'] = 'Affari'
+                m['body'] = rep_json["articles"][n]["content"]
+                m['rank'] = n
+                m.save!
+                affari += 1
+            end
+            if (cultura < 3 && keyword == "cultura")
+                m = Post.new
+                m['user_id'] = 1
+                m['title'] = rep_json["articles"][n]["title"]
+                m['category'] = 'Cultura'
+                m['body'] = rep_json["articles"][n]["content"]
+                m['rank'] = n
+                m.save!
+                cultura += 1
+            end
+            if (politica < 3 && keyword == "politica")
+                m = Post.new
+                m['user_id'] = 1
+                m['title'] = rep_json["articles"][n]["title"]
+                m['category'] = 'Politica'
+                m['body'] = rep_json["articles"][n]["content"]
+                m['rank'] = n
+                m.save!
+                politica += 1
+            end
+            if (spettacolo < 3 && keyword == "spettacoli")
+                m = Post.new
+                m['user_id'] = 1
+                m['title'] = rep_json["articles"][n]["title"]
+                m['category'] = 'Spettacoli'
+                m['body'] = rep_json["articles"][n]["content"]
+                m['rank'] = n
+                m.save!
+                spettacolo += 1
+            end
+            if (sport < 3 && keyword == "sport")
+                m = Post.new
+                m['user_id'] = 1
+                m['title'] = rep_json["articles"][n]["title"]
+                m['category'] = 'Affari'
+                m['body'] = rep_json["articles"][n]["content"]
+                m['rank'] = n
+                m.save!
+                sport += 1
+            end
+            if(affari == 3 && cultura == 3 && politica == 3 && spettacolo == 3 && sport == 3)
+                n = tot_res
+            else
+                n += 1
+            end
+        end
         return true
     end
 
