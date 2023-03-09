8c7345f10e4aeb248102103584131a7876c218c8
Martinaa9 <martyvln99@gmail.com>
2023-03-09 11:50:17 +0100

    edit update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
-    before_action :admin_only, :except => [ :show, :edit, :destroy]
+    before_action :admin_only, :except => [ :show, :edit, :destroy, :update]
   
     def index
       @users = User.all
     end
     
     def edit
+      @user = User.find(params[:id])
     end
     
     def show
@@ -18,10 +19,10 @@ class UsersController < ApplicationController
   
     def update
       @user = User.find(params[:id])
-      if @user.update_attributes(secure_params)
-        redirect_to users_path, :notice => "User updated."
+      if @user.update(user_params)
+        redirect_to  user_path(current_user.id), :notice => "User updated."
       else
-        redirect_to users_path, :alert => "Unable to update user."
+        redirect_to user_path(current_user.id), :alert => "Unable to update user."
       end
     end
     def destroy
@@ -33,13 +34,13 @@ class UsersController < ApplicationController
     private
     
     def admin_only
-        unless current_user.admin?
-            redirect_to posts_url, :alert => "Access denied."
-        end
-    end
+      unless current_user.admin?
+          redirect_to posts_url, :alert => "Access denied."
+      end
+  end
     
-    def secure_params
-        params.require(:user).permit(:role)
+    def user_params
+        params.require(:user).permit(:name, :surname, :username, :phone, :email)
     end
     
 end
