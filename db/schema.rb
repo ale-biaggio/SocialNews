5a5debe88ec73f5de44542d681f4272d8ca7e8f7
ale-biaggio <alessandrobiaggioli@gmail.com>
2023-03-23 12:11:19 +0100

    mailer funzionante

--- db/schema.rb
+++ db/schema.rb
@@ -123,9 +123,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_22_163856) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.string "provider"
     t.string "uid"
     t.integer "roles_mask"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
