324a2192be8a77ce6374b8d0abd768dc07d7b14b
ale-biaggio <alessandrobiaggioli@gmail.com>
2023-03-25 11:42:39 +0100

    testo email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def report_email(user_report)
     @user_report = user_report
-    mail(to: 'socialnews@gmail.com', from: @user_report.email, subject: 'Nuova segnalazione utente')
+    mail(to: 'socialnews@gmail.com', from: @user_report.email, subject: 'Nuova comunicazione utente')
   end
 end
